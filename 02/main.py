e57adc05d676e3941c9e273417625c7bba5efbf9
Lana <115147586+ricorett@users.noreply.github.com>
2024-08-05 17:40:15 +0300

    fixed 02 task

--- 02/main.py
+++ 02/main.py
@@ -4,15 +4,21 @@ with open("test_1.txt", "r") as file:
     for line in file:
         client_list.append(line.split())
 
-client_list = dict(client_list)
-client_dict = {key: int(value) for key, value in client_list.items()}
+# client_list = dict(client_list)
+client_dict = {key: int(value) for key, value in client_list}
 
 for k, v in client_dict.items():
     print(k, ": ", v // bonus_coffee)
 
 with open("max_bonus.txt", "w") as bonus_file:
-    for values in client_dict.values():
-        max_bonus = 0
-        if values > max_bonus:
-            max_bonus = values // bonus_coffee
-    bonus_file.write(str(max_bonus))
+    max_bonus = 0
+    clients = []
+    for key, values in client_dict.items():
+        current_bonus =  values // bonus_coffee
+        if current_bonus > max_bonus:
+            max_bonus = current_bonus
+            clients = [key]
+        elif current_bonus == max_bonus:
+            clients.append(key)
+
+    bonus_file.write(", ".join(clients))
