c57c365cf5d5385b56562e23f62902e5615d1181
Lana <115147586+ricorett@users.noreply.github.com>
2024-08-08 13:53:55 +0300

    add params and TypeError handling

--- 04/main.py
+++ 04/main.py
@@ -1,2 +1,4 @@
 def concatenation(string1, string2):
+    if not isinstance(string1, str) or not isinstance(string2, str):
+        raise TypeError("Both arguments must be strings")
     return string1 + string2
--- 04/tests.py
+++ 04/tests.py
@@ -2,6 +2,16 @@ import pytest
 from main import concatenation
 
 
+@pytest.mark.parametrize("str1, str2, expected", [
+    ("Hello", "World", "HelloWorld"),
+    ("", "", ""),
+    ("", "For the Horde!", "For the Horde!"),
+
+])
+def test_param(str1, str2, expected):
+    assert concatenation(str1, str2) == expected
+
+
 def test_sum_1():
     assert concatenation("Hello", "World") == "HelloWorld"
 
@@ -13,6 +23,14 @@ def test_sum_2():
 def test_sum_3():
     assert concatenation("", "For the Horde!") == "For the Horde!"
 
+
+def test_error():
+    with pytest.raises(TypeError, match="Both arguments must be strings"):
+        concatenation(1, "Hello")
+
+
+def test_not_eq():
+    assert concatenation("Hello", "World") != "Hello World"
 
 @pytest.mark.xfail(reason='Намеренный провал')
 def test_fail():
